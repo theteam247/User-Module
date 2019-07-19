87d654fa92acd2ff5085b499d2c11a4a0ab616a8
Zetsin <zetsin@gmail.com>
2019-07-19 23:21:50 +0800

    lint

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
-
 import crypto from "crypto";
 import nodemailer from "nodemailer";
 import express, { Response, Request, NextFunction, Router } from "express";
 import compression from "compression";
 import bodyParser from "body-parser";
 import { check, sanitize, validationResult } from "express-validator";
-import JWT, { RequestHandler, Options as JWTOptions } from "express-jwt"
-import jsonwebtoken, { SignOptions } from "jsonwebtoken"
-import { Sequelize, Options as SequelizeOptions, ModelAttributes} from "sequelize"
-import UserModel from './modals/user'
+import JWT, { RequestHandler, Options as JWTOptions } from "express-jwt";
+import jsonwebtoken, { SignOptions } from "jsonwebtoken";
+import {
+  Sequelize,
+  Options as SequelizeOptions,
+  ModelAttributes
+} from "sequelize";
+import UserModel from "./modals/user";
 
 export interface UserOptions {
   jwt: JWTOptions & SignOptions;
@@ -17,13 +20,13 @@ export interface UserOptions {
 }
 
 class User {
-  options: UserOptions;
+  public options: UserOptions;
 
-  sequelize: Sequelize;
-  middleware: RequestHandler;
-  router: Router;
+  public sequelize: Sequelize;
+  public middleware: RequestHandler;
+  public router: Router;
 
-  constructor(options: UserOptions) {
+  public constructor(options: UserOptions) {
     this.options = options;
 
     this.initSequlize();
@@ -32,17 +35,19 @@ class User {
   }
 
   private initSequlize() {
-    this.sequelize = this.options.sequelize instanceof Sequelize ? this.options.sequelize : (new Sequelize(this.options.sequelize));
+    this.sequelize =
+      this.options.sequelize instanceof Sequelize
+        ? this.options.sequelize
+        : new Sequelize(this.options.sequelize);
 
     UserModel.define({
       sequelize: this.sequelize,
       attributes: this.options.model
-    }).sync()
-
+    }).sync();
   }
 
   private initJWT() {
-    this.middleware = JWT(this.options.jwt)
+    this.middleware = JWT(this.options.jwt);
   }
 
   private initRouter() {
@@ -51,12 +56,13 @@ class User {
     // middleware that is specific to this router
     this.router.use(express.json());
     this.router.use(bodyParser.json());
-    this.router.use(bodyParser.urlencoded({
-      extended: true
-    }));
+    this.router.use(
+      bodyParser.urlencoded({
+        extended: true
+      })
+    );
     this.router.use(compression());
 
-    
     this.router.post("/signup", this.postSignup);
     this.router.post("/login", this.postLogin);
     this.router.post("/account", this.postAccount);
@@ -64,136 +70,185 @@ class User {
     this.router.post("/password/forgot", this.postForgotPassword);
     this.router.post("/password/reset", this.postResetPassword);
 
-    this.router.use((error: Error, req: Request, res: Response, next: NextFunction) => {
-      res.status(500).json({
-        error
-      })
-    })
+    this.router.use(
+      (error: Error, req: Request, res: Response, next: NextFunction) => {
+        res.status(500).json({
+          error
+        });
+      }
+    );
   }
 
-  private postSignup = async (req: Request, res: Response, next: NextFunction) => {
-    await check("email", "Email is not valid").isEmail().run(req);
-    await check("password", "Password must be at least 4 characters long").isLength({ min: 6 }).run(req);
-    await check("confirmPassword", "Passwords do not match").equals(req.body.password).run(req);
-    await sanitize("email").normalizeEmail({ gmail_remove_dots: false }).run(req);
+  private postSignup = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    await check("email", "Email is not valid")
+      .isEmail()
+      .run(req);
+    await check("password", "Password must be at least 4 characters long")
+      .isLength({ min: 6 })
+      .run(req);
+    await check("confirmPassword", "Passwords do not match")
+      .equals(req.body.password)
+      .run(req);
+    await sanitize("email")
+      .normalizeEmail({ gmail_remove_dots: false })
+      .run(req);
 
     const errors = validationResult(req);
-  
+
     if (!errors.isEmpty()) {
-      return res.status(400).json(errors)
+      return res.status(400).json(errors);
     }
 
     const userFound = await UserModel.findOne({
       where: {
         email: req.body.email
       }
-    })
+    });
 
-    if(userFound) {
+    if (userFound) {
       return res.status(400).json({
-        error: `Account with that email address already exists.`
-      })
+        error: "Account with that email address already exists."
+      });
     }
 
     const userCreated = await UserModel.create({
       email: req.body.email,
       password: req.body.password
-    })
-    
-    const { secret, ...jwtOptions } = this.options.jwt
-    jsonwebtoken.sign(userCreated.toJSON(), secret as string, jwtOptions, (err, token) => {
-      if(err) {
-        return res.status(400).json(err);
+    });
+
+    const { secret, ...jwtOptions } = this.options.jwt;
+    jsonwebtoken.sign(
+      userCreated.toJSON(),
+      secret as string,
+      jwtOptions,
+      (err, token) => {
+        if (err) {
+          return res.status(400).json(err);
+        }
+
+        res.json({
+          token
+        });
       }
+    );
+  };
 
-      res.json({
-        token,
+  private postLogin = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    await check("email")
+      .isEmail()
+      .run(req);
+    await check("password")
+      .isLength({
+        min: 6
       })
-    })
-  }
-
-  private postLogin = async (req: Request, res: Response, next: NextFunction) => {
-    await check("email").isEmail().run(req);
-    await check("password").isLength({
-      min: 6
-    }).run(req);
-    await sanitize("email").normalizeEmail({
-      gmail_remove_dots: false
-    }).run(req);
+      .run(req);
+    await sanitize("email")
+      .normalizeEmail({
+        gmail_remove_dots: false
+      })
+      .run(req);
 
     const errors = validationResult(req);
-  
+
     if (!errors.isEmpty()) {
-      return res.status(400).json(errors)
+      return res.status(400).json(errors);
     }
 
-
-    const { email, password } = req.body
+    const { email, password } = req.body;
     const user = await UserModel.findOne({
       where: {
         email: email.toLowerCase()
       }
-    })
+    });
 
-    if(!user) {
+    if (!user) {
       return res.status(400).json({
         error: `Email ${email} not found.`
-      })
+      });
     }
 
-    if(!user.comparePassword(password)) {
+    if (!user.comparePassword(password)) {
       return res.status(400).json({
-        error: `Invalid email or password.`
-      })
+        error: "Invalid email or password."
+      });
     }
 
-    const { secret, ...jwtOptions } = this.options.jwt
-    jsonwebtoken.sign(user.toJSON(), secret as string, jwtOptions, (err, token) => {
-      if(err) {
-        return res.status(400).json(err);
+    const { secret, ...jwtOptions } = this.options.jwt;
+    jsonwebtoken.sign(
+      user.toJSON(),
+      secret as string,
+      jwtOptions,
+      (err, token) => {
+        if (err) {
+          return res.status(400).json(err);
+        }
+
+        res.json({
+          token
+        });
       }
+    );
+  };
 
-      res.json({
-        token,
+  private postAccount = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {};
+  private deleteAccount = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {};
+
+  private postForgotPassword = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    await check("email")
+      .isEmail()
+      .run(req);
+    await sanitize("email")
+      .normalizeEmail({
+        gmail_remove_dots: false
       })
-    })
-  }
-
-  private postAccount = async (req: Request, res: Response, next: NextFunction) => {}
-  private deleteAccount = async (req: Request, res: Response, next: NextFunction) => {}
-
-  private postForgotPassword = async (req: Request, res: Response, next: NextFunction) => {
-    await check("email").isEmail().run(req);
-    await sanitize("email").normalizeEmail({
-      gmail_remove_dots: false
-    }).run(req);
+      .run(req);
 
     const errors = validationResult(req);
-  
+
     if (!errors.isEmpty()) {
-      return res.status(400).json(errors)
+      return res.status(400).json(errors);
     }
 
     // createRandomToken
     const token = await new Promise<string>((resolve, reject) => {
       crypto.randomBytes(16, (err, buf) => {
-        if(err) {
-          return reject(err)
+        if (err) {
+          return reject(err);
         }
         return resolve(buf.toString("hex"));
-      })
-    })
+      });
+    });
 
     // setRandomToken
     const user = await UserModel.findOne({
       where: {
         email: req.body.email
       }
-    })
+    });
     if (!user) {
       return res.status(400).json({
-        error: `Account with that email address does not exist.`
-      })
+        error: "Account with that email address does not exist."
+      });
     }
 
     const passwordResetExpires = new Date();
@@ -211,18 +266,21 @@ class User {
         user: process.env.SENDGRID_USER,
         pass: process.env.SENDGRID_PASSWORD
       }
-    })
-    
+    });
+
     await transporter.sendMail({
       to: user.email,
       from: process.env.SENDGRID_FROM,
-      subject: `Reset your password on Hackathon Starter`,
+      subject: "Reset your password on Hackathon Starter",
       text: `http://${req.headers.host}/reset/${token}`
-    })
+    });
+  };
 
-  }
-
-  private postResetPassword = async (req: Request, res: Response, next: NextFunction) => {}
+  private postResetPassword = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {};
 }
 
-export default User
+export default User;
--- src/modals/user.ts
+++ src/modals/user.ts
@@ -1,100 +1,118 @@
-import bcryptjs from "bcryptjs"
-import { INTEGER, STRING, TEXT, DATE, Model, Sequelize, ModelAttributes, ModelOptions } from "sequelize";
+import bcryptjs from "bcryptjs";
+import {
+  INTEGER,
+  STRING,
+  TEXT,
+  DATE,
+  Model,
+  Sequelize,
+  ModelAttributes,
+  ModelOptions
+} from "sequelize";
 
 class User extends Model {
   private password!: string;
-  id!: number;
-  email!: string;
-  name: string;
-  gender: string;
-  picture: string;
+  public id!: number;
+  public email!: string;
+  public name: string;
+  public gender: string;
+  public picture: string;
 
-  activateToken: string;
-  activateTokenExpires: Date;
-  passwordResetToken: string;
-  passwordResetExpires: Date;
+  public activateToken: string;
+  public activateTokenExpires: Date;
+  public passwordResetToken: string;
+  public passwordResetExpires: Date;
 
-  toJSON() {
-    const { password, activateToken, activateTokenExpires, passwordResetToken, passwordResetExpires, ...rest } = this.get() as User
-    return rest
+  public toJSON() {
+    const {
+      password,
+      activateToken,
+      activateTokenExpires,
+      passwordResetToken,
+      passwordResetExpires,
+      ...rest
+    } = this.get() as User;
+    return rest;
   }
 
-  comparePassword = async (candidatePassword: string) => {
+  public comparePassword = async (candidatePassword: string) => {
     return await bcryptjs.compare(candidatePassword, this.password);
-  }
+  };
 
-  static define(opts: {
-    sequelize: Sequelize,
-    attributes?: ModelAttributes,
-    options?: ModelOptions,
+  public static define(opts: {
+    sequelize: Sequelize;
+    attributes?: ModelAttributes;
+    options?: ModelOptions;
   }) {
-    User.init({
-      id: {
-        type: INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-      },
-      email: {
-        type: STRING,
-        allowNull: false,
-        unique: true,
-      },
-      // hash
-      password: {
-        type: TEXT,
-        allowNull: false
-      },
-    
-      // profile
-      name: {
-        type: STRING,
-        allowNull: true
-      },
-      gender: {
-        type: STRING,
-        allowNull: true
-      },
-      picture: {
-        type: STRING,
-        allowNull: true
-      },
-    
-      // send email with activateToken after registration
-      activateToken: {
-        type: STRING,
-        allowNull: true
-      },
-      activateTokenExpires: {
-        type: DATE,
-        allowNull: true
-      },
-    
-      // send email with passwordResetToken after reqeusting to reset password
-      passwordResetToken: {
-        type: STRING,
-        allowNull: true
-      },
-      passwordResetExpires: {
-        type: DATE,
-        allowNull: true
-      },
+    User.init(
+      {
+        id: {
+          type: INTEGER,
+          autoIncrement: true,
+          primaryKey: true
+        },
+        email: {
+          type: STRING,
+          allowNull: false,
+          unique: true
+        },
+        // hash
+        password: {
+          type: TEXT,
+          allowNull: false
+        },
 
-      // extra attrs
-      ...opts.attributes
+        // profile
+        name: {
+          type: STRING,
+          allowNull: true
+        },
+        gender: {
+          type: STRING,
+          allowNull: true
+        },
+        picture: {
+          type: STRING,
+          allowNull: true
+        },
 
-    }, {
-      sequelize: opts.sequelize,
-      ...opts.options,
-      hooks: {
-        ...(opts.options || {}).hooks,
-        beforeCreate: async (user: User) => {
-          user.password = await bcryptjs.hash(user.password, 10)
+        // send email with activateToken after registration
+        activateToken: {
+          type: STRING,
+          allowNull: true
+        },
+        activateTokenExpires: {
+          type: DATE,
+          allowNull: true
+        },
+
+        // send email with passwordResetToken after reqeusting to reset password
+        passwordResetToken: {
+          type: STRING,
+          allowNull: true
+        },
+        passwordResetExpires: {
+          type: DATE,
+          allowNull: true
+        },
+
+        // extra attrs
+        ...opts.attributes
+      },
+      {
+        sequelize: opts.sequelize,
+        ...opts.options,
+        hooks: {
+          ...(opts.options || {}).hooks,
+          beforeCreate: async (user: User) => {
+            user.password = await bcryptjs.hash(user.password, 10);
+          }
         }
       }
-    })
+    );
 
-    return User
+    return User;
   }
 }
 
-export default User
+export default User;
